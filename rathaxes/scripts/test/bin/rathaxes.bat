eb338170c577e9d93b4a6571a2464942ba9d6130
Louis Opter <louis@lse.epitech.net>
2011-07-21 00:50:26 -0700

    build: on Windows add a Rathaxes Shell and shortcut to easily execute rathaxes

---
+++ rathaxes/scripts/test/bin/rathaxes.bat
@@ -0,0 +1,6 @@
+@echo off
+
+rem We have to explicitely give the path of the script as the first argument on
+rem Windows.
+
+%CODEWORKER% --quiet %INCLUDE_PATHS% -script %COMPILER_PATH%/rathaxes.cws -args %COMPILER_PATH%/rathaxes.cws %*
