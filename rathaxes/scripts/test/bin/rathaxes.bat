ddd92a7802b82c50ff1ea4fc84204903a5e8735c
David Pineau <dav.pineau@gmail.com>
2011-09-29 15:53:18 +0200

    rathaxes: Link: Added some rathaxes command manipulating the cache
     * Fixed .bat windows launching script
     * Added two commands to the rathaxes main script : clean and install.

--- rathaxes/scripts/test/bin/rathaxes.bat
+++ rathaxes/scripts/test/bin/rathaxes.bat
@@ -3,4 +3,4 @@
 rem We have to explicitely give the path of the script as the first argument on
 rem Windows.
 
-%CODEWORKER% --quiet %INCLUDE_PATHS% -script %COMPILER_PATH%/rathaxes.cws -args %COMPILER_PATH%/rathaxes.cws %*
+%CODEWORKER% --quiet %INCLUDE_PATHS% -script %COMPILER_PATH%/rathaxes.cws -args %COMPILER_PATH%/rathaxes.cws %LOCAL_CACHE_PATH%/ %*
