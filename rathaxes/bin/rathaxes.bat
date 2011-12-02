3740f491470e1c59b5e8d402f702bb72e1cb1f1a
Louis Opter <louis@lse.epitech.net>
2011-12-02 16:06:29 +0100

    rathaxes: archive (remove) Rathaxes 2009 and move rathaxes/scrips/test/* directly into rathaxes/

--- rathaxes/bin/rathaxes.bat
+++ rathaxes/bin/rathaxes.bat
@@ -1,23 +1,6 @@
 @echo off
-rem Script d'execution du compilateur Rathaxes
 
-SET OS=windows
-SET INSTALLPATH=c:\rathaxes\rathaxes\
-SET GENERATIONPATH=%INSTALLPATH%\scripts\generation\
-SET MISCPATH=%INSTALLPATH%\scripts\misc\
-SET PARSINGPATH=%INSTALLPATH%\scripts\parsing\
-SET CODEWORKEREXTENSION=%INSTALLPATH%\scripts\CodeWorkerExtension\
-SET SEMANTICSPATH=%INSTALLPATH%\scripts\semantics\
-SET COREPATH=%INSTALLPATH%\scripts\core\
-SET CNORM_PATH=%INSTALLPATH%\scripts\cnorm\
-SET CNORM2PATH=%INSTALLPATH%\scripts\cnorm\cnorm2c\
-SET BLACKLIBRARYPATH=%INSTALLPATH%\black_library\
-SET INCLUDEPATH=-I %GENERATIONPATH% -I %MISCPATH% -I %PARSINGPATH% -I %CODEWORKEREXTENSION% -I %SEMANTICSPATH% -I %COREPATH% -I %CNORM_PATH% -I %CNORM2PATH% -I %BLACKLIBRARYPATH%
+rem We have to explicitely give the path of the script as the first argument on
+rem Windows.
 
-IF "%1"=="clean" del *.c del *.rtree goto exit
-
-codeworker -nologo -stack 1000 %INCLUDEPATH% -script %INSTALLPATH%\scripts\rathaxes.cws -args "windows" %INSTALLPATH% "windows" %1 %2 %3 %4 %5 %6 %7 %8 %9
-
-pause
-:exit
-
+%CODEWORKER% --quiet %INCLUDE_PATHS% -script %COMPILER_PATH%/rathaxes.cws -args %COMPILER_PATH%/rathaxes.cws %LOCAL_CACHE_PATH% %*
