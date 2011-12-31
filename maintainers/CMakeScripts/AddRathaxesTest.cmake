76eb6881a90eb855191d6e5adc2d5a1755782241
Louis Opter <louis@lse.epitech.net>
2011-12-31 19:34:11 +0100

    maintainers: add a new CMake "library" UseRathaxes.cmake to add a minimal Rathaxes support into CMake
    - Add the new ADD_RATHAXES_EXECUTABLE function to build a .rtx target;
    - No more generated files in the source directory;
    - No more tedious generate shell script to write;
    - ADD_RATHAXES_TEST is renamed to ADD_RATHAXES_CWS_TEST.

--- maintainers/CMakeScripts/AddRathaxesTest.cmake
+++
@@ -1,8 +0,0 @@
-# This function call codeworker + cnorm with the given codeworker script.
-#
-# The codeworker script must return 0 on success.
-
-FUNCTION(ADD_RATHAXES_TEST CW_SCRIPT)
-    STRING(REGEX MATCH "(.*)\\.cws$" TEST_NAME "${CW_SCRIPT}")
-    ADD_TEST("Rathaxes_${TEST_NAME}" ${CODEWORKER_BINARY_PATH} "-I" "${RATHAXES_SOURCE_DIR}/maintainers/cnorm/src" "-I" "${RATHAXES_SOURCE_DIR}/rathaxes/compiler/" "-script" "${CMAKE_CURRENT_SOURCE_DIR}/${CW_SCRIPT}" "-args" "${RATHAXES_SOURCE_DIR}/rathaxes")
-ENDFUNCTION(ADD_RATHAXES_TEST CW_SCRIPT)
